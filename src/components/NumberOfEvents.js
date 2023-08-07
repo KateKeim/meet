19449f220bd484a59883098d45c8e41c5aae00a5
KateKeim <110600575+KateKeim@users.noreply.github.com>
2023-08-07 11:20:49 +0200

    Update Error Alert

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -15,21 +15,24 @@
 
 export const NumberOfEvents = ({ setCurrentNOE, setErrorAlert }) => {
   const handleInputChanged = (event) => {
-      const value = event.target.value
+    const value = (event.target.value <=50 && event.target.value > 0? (
+      event.target.value
+      ) : event.target.value <=0 ? (
+      0
+      ) : 50)
       setCurrentNOE(value)
-      setErrorAlert("")
-
       let errorText;
-      if (value.length <= 0 || isNaN(value)) {
-        errorText = "Only positive numbers are allowed"
-      } else if (value > 50) {
-        errorText = "Could not show more than 50 events"
-      } 
-      else {
-        errorText = ""
+      if(isNaN(event.target.value) === true) {
+      errorText = "Number of events must contain only numbers"
+      } else if (event.target.value <= 0) {
+      errorText = "Number of events must be greater than 0"
+      } else if (event.target.value > 50) {
+      errorText = "The maximum number of events is 50"
+      } else {
+      errorText = ""
       }
-      setErrorAlert(errorText);
-  }
+      setErrorAlert(errorText)
+      }
 
 //   return (
 //     <div id="number-of-events">
